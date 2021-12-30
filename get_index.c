b27ec3691132479a11e3da06087fcdec3b0fd38b
Brahim  Maaqoul <bmaaqoul@e2r12p3.1337.ma>
2021-12-30 21:07:01 +0100

    fixing the timeout

--- get_index.c
+++ get_index.c
@@ -6,7 +6,7 @@
 /*   By: bmaaqoul <bmaaqoul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/30 13:43:04 by bmaaqoul          #+#    #+#             */
-/*   Updated: 2021/12/30 14:43:23 by bmaaqoul         ###   ########.fr       */
+/*   Updated: 2021/12/30 21:06:43 by bmaaqoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,12 @@ int	get_index(t_list **head)
 	int		i;
 	int		j;
 
-	prev->next = *head;
-	prev = prev->next;
+	prev = *head;
 	if ((*head)->next == NULL)
-		return (0);
+		return (1);
 	*head = (*head)->next;
-	i = 1;
-	j = 0;
+	i = 2;
+	j = 1;
 	while (*head)
 	{
 		if ((*head)->num < prev->num)
@@ -38,23 +37,23 @@ int	get_index(t_list **head)
 	return (j);
 }
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(void)
-{
-	t_list	*node;
+// int main(void)
+// {
+// 	t_list	*node;
 
-	node = ft_lstnew(1);
-	node->next = ft_lstnew(0);
-	node->next->next = ft_lstnew(-50000);
-	 node->next->next->next = ft_lstnew(1000);
-	 node->next->next->next->next = ft_lstnew(-222111);
-	 node->next->next->next->next->next = ft_lstnew(-222111);
-	 printf("%d\n", node->num);
-	 printf("%d\n", node->next->num);
-	printf("%d\n", node->next->next->num);
-	 printf("%d\n", node->next->next->next->num);
-	 printf("%d\n", node->next->next->next->next->num);
-	 printf("%d\n",node->next->next->next->next->next->num);
-	printf("\n%d\n", get_index(&node));
-}
+// 	node = ft_lstnew(1);
+// 	node->next = ft_lstnew(0);
+// 	node->next->next = ft_lstnew(-50000);
+// 	//  node->next->next->next = ft_lstnew(1000);
+// 	//  node->next->next->next->next = ft_lstnew(-222111);
+// 	//  node->next->next->next->next->next = ft_lstnew(-2221111);
+// 	//  printf("%d\n", node->num);
+// 	//  printf("%d\n", node->next->num);
+// 	// printf("%d\n", node->next->next->num);
+// 	//  printf("%d\n", node->next->next->next->num);
+// 	//  printf("%d\n", node->next->next->next->next->num);
+// 	//  printf("%d\n",node->next->next->next->next->next->num);
+// 	printf("\n%d\n", get_index(&node));
+// }
